4966c79a6297821accdd1557bfb8fd40d17567ff
mmacarons <manymany.macarons@gmail.com>
2018-12-20 14:52:15 +0900

    이벤트 환경 구성

--- app/src/main/java/com/example/mira/dominopizza/MainActivity.java
+++ app/src/main/java/com/example/mira/dominopizza/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.mira.dominopizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+
+
     }
 }
