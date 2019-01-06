46bdd2347b79cd5b893baf4f08f3fbe8ffccbcf1
mmacarons <manymany.macarons@gmail.com>
2019-01-06 21:54:12 +0900

    공지 화면 구성 완성

---
+++ app/src/main/java/com/example/mira/dominopizza/NoticeActivity.java
@@ -0,0 +1,31 @@
+package com.example.mira.dominopizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
